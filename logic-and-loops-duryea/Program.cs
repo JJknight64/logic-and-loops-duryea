488640d69d9d3577e05bcb6a20671a05a07841a7
JJknight64 <jwd8744@rit.edu>
2024-10-03 08:37:14 -0400

    Wrote the intro text, user input with basic options depending on what they put in

--- logic-and-loops-duryea/Program.cs
+++ logic-and-loops-duryea/Program.cs
@@ -1,10 +1,67 @@
-﻿namespace logic_and_loops_duryea
+﻿// Jacob Duryea
+// 10/3/2024
+// Interactive Des. and Alg. Prob. Sol. III
+// Logic and Loops
+
+namespace logic_and_loops_duryea
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string playerName = "adventurer";
+            string userChoice = "NONE";
+
+            // intro text
+            Console.Write("Welcome to the jungle \nWe got fun and games \nWe got everything you want \nHoney, we know the names \n \n");
+            Console.WriteLine("Wait... I don't actually know your name.");
+            Console.WriteLine("What is your name?");
+            Console.WriteLine("[Please enter your name, then press 'ENTER' on the keyboard]");
+
+            // enter your name
+            playerName = Console.ReadLine();
+            Console.WriteLine("\nWelcome to the jungle, " + playerName + "!");
+
+            // continue the story
+            Console.WriteLine(
+                "We approach a clearing in the jungle " +
+                "\nThere appears to be an trail to the left... " +
+                "\nAnd a cave entrance to the right."
+                );
+
+            // narrator talking
+            Console.WriteLine("Which path do you choose?");
+            Console.WriteLine("Type 'left' to walk the trail, or 'right' to enter the cave.");
+            // take the user input
+            userChoice = Console.ReadLine();
+            Console.WriteLine("\nYou chose: " + userChoice + "\n");
+
+            // your choices DO matter, actually!!
+            if (userChoice == "left")
+            {
+                // trail dialogue
+                Console.Write(
+                    "As you walk along the trail, you notice the vines and greenery start to thin out.\n" +
+                    "The sun begins to shine through the tops of the trees more and more as you trudge along this muddy path.\n" +
+                    "Eventually, you trip over an inconvenient rock stuck in the mud and then..." +
+                    "You look up to notice that the jungle is behind you! Congratulations, " + playerName + ", you've escaped!\n");
+            }
+            else if(userChoice == "right")
+            {
+                // cave dialogue
+                Console.Write(
+                    "You steel yourself as you decide to brave the dark and dreary cave to your right.\n" +
+                    "The further along you progress, the more you think that this might've been a bad idea...\n" +
+                    "A swarm of bats suddenly startles awake at your presence and scrambles out of the cave all at once, temporarily obscuring your vision completely.\n" +
+                    "When you uncover your eyes, you notice something shiny in the darkness...\n" +
+                    "By gosh, it seems you've found it! The lost treasure of the jungle! Congratulations, " + playerName + "!\n");
+            }
+            else
+            {
+                // you done messed up!
+                Console.Write(
+                    "Two choices, bub. Restart and try again.\n");
+            }
         }
     }
 }
